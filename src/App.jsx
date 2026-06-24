660707e1a4f003bdda295f74c9294259bc67fdf1
kksha7300 <krishnakantsharmavlogs@gmail.com>
2026-06-24 18:20:45 +0530

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,12 @@ function App() {
   return (
     <center class="todo-container">
       <h1 style={{'background':'yellow'}}>Todo App </h1>
+      <div class="container text-center">
+  <div class="row">
+    <div class="col-8">col-8</div>
+    <div class="col-4">col-4</div>
+  </div>
+</div>
     </center>
   )
 }
