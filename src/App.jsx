d43b8b6dfaee5c49130237bb4f1cc9a7ba157de1
kksha7300 <krishnakantsharmavlogs@gmail.com>
2026-06-24 18:24:50 +0530

    update layout

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,9 @@ function App() {
       <h1 style={{'background':'yellow'}}>Todo App </h1>
       <div class="container text-center">
   <div class="row">
-    <div class="col-8">col-8</div>
-    <div class="col-4">col-4</div>
+    <div class="col-8">col-6</div>
+    <div class="col-8">col-4</div>
+    <div class="col-4">col-2</div>
   </div>
 </div>
     </center>
